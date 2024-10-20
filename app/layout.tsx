7ef58b85cbe8a43bbc7a3d9950bf24119bf81a50
Dominic Camill <camilldominic@gmail.com>
2024-10-20 16:35:31 -0700

    got MUI components to work properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,21 @@ import "./globals.css";
 import { NavBar } from "@/components/components";
 import styles from "@/app/page.module.css"
 import Link from "next/link";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import CssBaseline from '@mui/material/CssBaseline';
+// import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dominic's Portfolio",
   description: "A portfolio website created by Dominic Camill",
-  icons: {
-    other: {
-      rel: "stylesheet",
-      url: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    }
-  }
+  // icons:{
+  //   other: {
+  //     rel: "stylesheet",
+  //     url: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+  //   }
+  // }
 };
 
 export default function RootLayout({
@@ -25,24 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </Head> */}
       <body className={inter.className}>
-        {/* <header className={styles.header}>
-			  	<NavBar />
-    	  </header> */}
-        {children}
-        {/* <footer>
-				  <div className="footerPages">
-				  	<h3>
-				  		Pages
-				  	</h3>
-				  	<ul className={styles["footerPageList"]}>
-				  		<li><Link id={"about"} href="" >About Me</Link></li>
-				  		<li><Link id={"interests"} href="" >Interests</Link></li>
-				  		<li><Link id={"projects"} href="" >Projects</Link></li>
-				  		<li><Link id={"contacts"} href="" >Contact Me</Link></li>
-				  	</ul>
-				  </div>
-    		</footer> */}
+        <AppRouterCacheProvider>
+          <CssBaseline />
+          {children}
+        </AppRouterCacheProvider>   
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,109 +1,137 @@
-import Image from "next/image";
-import Head from "next/head";
-import styles from "./page.module.css";
-import 'material-symbols';
-import { Header, Footer, BodyChunk } from "@/components/components";
-import Link from "next/link";
-import HeaderBackground from "@/public/images/HeaderBackgroundGrid02.png"
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button"
-// import Icon from "../components/icon";
+// import * as React from 'react';
+// import Image from "next/image";
+// import Head from "next/head";
+// import styles from "./page.module.css";
+// import 'material-symbols';
+// import { Header, Footer, BodyChunk } from "@/components/components";
+// import Link from "next/link";
+// import HeaderBackground from "@/public/images/HeaderBackgroundGrid02.png"
+// import Stack from "@mui/material/Stack";
+// import Button from "@mui/material/Button"
+// // import Icon from "../components/icon";
 
-export default function Home() {
-	return(
-		<Stack spacing={2} direction="row">
-    		<Button variant="text">Text</Button>
-    		<Button variant="contained">Contained</Button>
-    		<Button variant="outlined">Outlined</Button>
-    	</Stack>
-	);
-    // return (
-	// 	<main className={styles["main"]}>
-    //        	{/* <header>
-	// 			<NavBar />
-    //     		<nav className={styles["navBar"]}>
-    //        			<a className={styles["navBarElem"]} id="home" href="">
-	// 					<Icon newClass={"navBarElemIcon"} iconName={"home"} />
-    //        			    <div className={styles["navButton"]}>
-    //        	    		    <p>Home</p>
-    //             		    <div></div>
-    //             		</div>
-    //         		</a>
-    //         		<a className={styles["navBarElem"]} id="about" href="/aboutme">
-	// 					<Icon newClass={"navBarElemIcon"} iconName={"person"} />
-    //             		<div className={styles["navButton"]}>
-    //             		    <p>About Me</p>
-    //             		    <div></div>
-    //             		</div>
-    //         		</a>
-    //         		<a className={styles["navBarElem"]} id="interest" href="/interests">
-    //             		<Icon newClass={"navBarElemIcon"} iconName={"interests"} />
-    //             		<div className={styles["navButton"]}>
-    //             		    <p>Interests</p>
-    //             		    <div></div>
-    //             		</div>
-    //         		</a>
-    //         		<a className={styles["navBarElem"]} id="contact" href="/contact">
-	// 					<Icon newClass={"navBarElemIcon"} iconName={"alternate_email"} />
-    //    	        		<div className={styles["navButton"]}>
-    //  		     		    <p>Contact Me</p>
-    //    	        		    <div></div>
-    //    	        		</div>
-    //    	    		</a>
-    //    			</nav>
-    //    			<div className={styles["headerTitle"]}>
-    //         		<Image className={styles["headerImage"]} width={1920} height={1080} src={HeaderBackground} alt="Header Background Image" />
-    //         		<h1 className={styles["headerText"]}>
-    //         		    Dominic<br />Camill
-    //         		</h1>
-    //     		</div>
-    // 		</header> */}
-	// 		<Header title={"Dominic Camill"} imgSrc={HeaderBackground}/>
-    // 		<div className={styles["mainSection"]}>
-	// 			<BodyChunk
-	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
-	// 			title={"About Me"}
-	// 			variation={"1"}
-	// 			imgSrc={"person"}
-	// 			redirectLink="/aboutme"
-	// 			/>
-	// 			<BodyChunk
-	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
-	// 			title={"Interests"}
-	// 			variation={"2"}
-	// 			imgSrc={"interests"}
-	// 			redirectLink="/interests"
-	// 			/>
-	// 			<BodyChunk
-	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
-	// 			title={"Projects"}
-	// 			variation={"1"}
-	// 			imgSrc={"assignment"}
-	// 			redirectLink="/projects"
-	// 			/>
-	// 			<BodyChunk
-	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
-	// 			title={"Contact Me"}
-	// 			variation={"2"}
-	// 			imgSrc={"alternate_email"}
-	// 			redirectLink="/contact"
-	// 			/>
-    // 		</div>
-	// 		<Footer />
-    // 		{/* <footer>
-	// 			<div className={styles.footerPages}>
-	// 				<h3>
-	// 					Pages
-	// 				</h3>
-	// 				<div className={styles.footerPageList}>
-	// 					<Link id={"Home"} href="/" >Home</Link>
-	// 					<Link id={"about"} href="/aboutme" >About Me</Link>
-	// 					<Link id={"interests"} href="/interests" >Interests</Link>
-	// 					<Link id={"projects"} href="/projects" >Projects</Link>
-	// 					<Link id={"contacts"} href="/contact" >Contact Me</Link>
-	// 				</div>
-	// 			</div>
-    // 		</footer> */}
-    // 	</main>
-    // );
+// export default function Home() {
+// 	return(
+// 		<Stack spacing={2} direction="row">
+//     		<Button className={styles["navButton"]} variant="text">Text</Button>
+//     		<Button className={styles["navButton"]} variant="contained">Contained</Button>
+//     		<Button className={styles["navButton"]} variant="outlined">Outlined</Button>
+//     	</Stack>
+// 	);
+//     // return (
+// 	// 	<main className={styles["main"]}>
+//     //        	{/* <header>
+// 	// 			<NavBar />
+//     //     		<nav className={styles["navBar"]}>
+//     //        			<a className={styles["navBarElem"]} id="home" href="">
+// 	// 					<Icon newClass={"navBarElemIcon"} iconName={"home"} />
+//     //        			    <div className={styles["navButton"]}>
+//     //        	    		    <p>Home</p>
+//     //             		    <div></div>
+//     //             		</div>
+//     //         		</a>
+//     //         		<a className={styles["navBarElem"]} id="about" href="/aboutme">
+// 	// 					<Icon newClass={"navBarElemIcon"} iconName={"person"} />
+//     //             		<div className={styles["navButton"]}>
+//     //             		    <p>About Me</p>
+//     //             		    <div></div>
+//     //             		</div>
+//     //         		</a>
+//     //         		<a className={styles["navBarElem"]} id="interest" href="/interests">
+//     //             		<Icon newClass={"navBarElemIcon"} iconName={"interests"} />
+//     //             		<div className={styles["navButton"]}>
+//     //             		    <p>Interests</p>
+//     //             		    <div></div>
+//     //             		</div>
+//     //         		</a>
+//     //         		<a className={styles["navBarElem"]} id="contact" href="/contact">
+// 	// 					<Icon newClass={"navBarElemIcon"} iconName={"alternate_email"} />
+//     //    	        		<div className={styles["navButton"]}>
+//     //  		     		    <p>Contact Me</p>
+//     //    	        		    <div></div>
+//     //    	        		</div>
+//     //    	    		</a>
+//     //    			</nav>
+//     //    			<div className={styles["headerTitle"]}>
+//     //         		<Image className={styles["headerImage"]} width={1920} height={1080} src={HeaderBackground} alt="Header Background Image" />
+//     //         		<h1 className={styles["headerText"]}>
+//     //         		    Dominic<br />Camill
+//     //         		</h1>
+//     //     		</div>
+//     // 		</header> */}
+// 	// 		<Header title={"Dominic Camill"} imgSrc={HeaderBackground}/>
+//     // 		<div className={styles["mainSection"]}>
+// 	// 			<BodyChunk
+// 	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
+// 	// 			title={"About Me"}
+// 	// 			variation={"1"}
+// 	// 			imgSrc={"person"}
+// 	// 			redirectLink="/aboutme"
+// 	// 			/>
+// 	// 			<BodyChunk
+// 	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
+// 	// 			title={"Interests"}
+// 	// 			variation={"2"}
+// 	// 			imgSrc={"interests"}
+// 	// 			redirectLink="/interests"
+// 	// 			/>
+// 	// 			<BodyChunk
+// 	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
+// 	// 			title={"Projects"}
+// 	// 			variation={"1"}
+// 	// 			imgSrc={"assignment"}
+// 	// 			redirectLink="/projects"
+// 	// 			/>
+// 	// 			<BodyChunk
+// 	// 			content={"Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent conubia sociosqu dolor sociosqu; turpis nascetur pellentesque. Nullam quis suspendisse dis dictum inceptos posuere."}
+// 	// 			title={"Contact Me"}
+// 	// 			variation={"2"}
+// 	// 			imgSrc={"alternate_email"}
+// 	// 			redirectLink="/contact"
+// 	// 			/>
+//     // 		</div>
+// 	// 		<Footer />
+//     // 		{/* <footer>
+// 	// 			<div className={styles.footerPages}>
+// 	// 				<h3>
+// 	// 					Pages
+// 	// 				</h3>
+// 	// 				<div className={styles.footerPageList}>
+// 	// 					<Link id={"Home"} href="/" >Home</Link>
+// 	// 					<Link id={"about"} href="/aboutme" >About Me</Link>
+// 	// 					<Link id={"interests"} href="/interests" >Interests</Link>
+// 	// 					<Link id={"projects"} href="/projects" >Projects</Link>
+// 	// 					<Link id={"contacts"} href="/contact" >Contact Me</Link>
+// 	// 				</div>
+// 	// 			</div>
+//     // 		</footer> */}
+//     // 	</main>
+//     // );
+// }
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
+
+export default function FloatingActionButtons() {
+  return (
+    <Box sx={{ '& > :not(style)': { m: 1 } }}>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit">
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended">
+        <NavigationIcon sx={{ mr: 1 }} />
+        Navigate
+      </Fab>
+      <Fab disabled aria-label="like">
+        <FavoriteIcon />
+      </Fab>
+    </Box>
+  );
 }
